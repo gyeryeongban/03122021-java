ed938c109ce5acdb99dbd7962780b6a165951ed7
Gyeryeong Ban <gyeryeong.ban@outlook.com>
2021-03-13 17:04:48 +0900

    Output

---
+++ Q1001-1008/src/App.java
@@ -0,0 +1,27 @@
+public class App {
+    public static void main(String[] args) {
+        // 1001
+        System.out.println("Hello");
+
+        // 1002
+        System.out.println("Hello World");
+
+        // 1003
+        System.out.println("Hello\nWorld");
+
+        // 1004
+        System.out.println("\'Hello\'");
+
+        // 1005
+        System.out.println("\"Hello World\"");
+
+        // 1006
+        System.out.println("\"!@#$%^&*()\"");
+
+        // 1007
+        System.out.println("\"C:\\Download\\hello.cpp\"");
+
+        // 1008
+        System.out.println("\u250C\u252C\u2510\n\u251C\u253C\u2524\n\u2514\u2534\u2518");
+    }
+}
