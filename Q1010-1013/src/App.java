458de883aaa82ef6d1bcbd1b3ba6ce98f5ea3c67
Gyeryeong Ban <gyeryeong.ban@outlook.com>
2021-03-13 17:08:17 +0900

    Input/Output

---
+++ Q1010-1013/src/App.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 1010
+        int a = sc.nextInt(15);
+        System.out.println(a);
+
+        // 1011
+        String b = sc.next("p");
+        System.out.println(b);
+
+        // 1012
+        float c = sc.nextFloat(1.414213);
+        System.out.println(c);
+
+        // 1013
+        int d = sc.nextInt(), e = sc.nextInt();
+        System.out.println(d + " " + e);
+    }
+}
