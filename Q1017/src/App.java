847ea4426a8fdb3cbb2fc310865f5ad7ae24b94b
Gyeryeong Ban <gyeryeong.ban@outlook.com>
2021-03-15 14:32:09 +0900

    Input/Output

---
+++ Q1017/src/App.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(125);
+        System.out.println(a + " " + a + " " + a);
+        sc.close();
+    }
+}
