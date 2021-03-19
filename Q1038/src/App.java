8f9d379f169d0801ba1f6923717d0e29dd2dd41b
Gyeryeong Ban <gyeryeong.ban@outlook.com>
2021-03-19 11:08:03 +0900

    Arithmetic Operation

---
+++ Q1038/src/App.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextInt(), b = sc.nextInt();
+        System.out.println(a + b);
+        sc.close();
+    }
+}
