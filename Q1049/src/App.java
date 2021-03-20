50cc6fba25f3a8595dfae386193a08015910a3ad
Gyeryeong Ban <gyeryeong.ban@outlook.com>
2021-03-20 14:29:07 +0900

    Comparative Operation

---
+++ Q1049/src/App.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(), b = sc.nextInt();
+        if (a > b)
+            System.out.println(1);
+        else if (a <= b)
+            System.out.println(0);
+        sc.close();
+    }
+}
